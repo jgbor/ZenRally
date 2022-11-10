0f2fffd6b244ae0c71b0662a3cb955760ddedc57
OctaPinball <scholtz.balint@gmail.com>
2022-11-10 11:36:28 +0100

    Exit game

---
+++ Assets/Scripts/ExitScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitScript : MonoBehaviour
+{
+    public void Exit()
+    {
+        Debug.Log("EXIT");
+        Application.Quit();
+    }
+}
