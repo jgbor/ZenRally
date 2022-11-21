c38b282007be8ca2e4aeaa5652d402b50d186035
jgbor <100523032+jgbor@users.noreply.github.com>
2022-11-21 18:51:34 +0100

    start alakítás

--- Assets/Scripts/CarSpawn.cs
+++ Assets/Scripts/CarSpawn.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CarSpawn : MonoBehaviour
 {
@@ -9,6 +10,12 @@ public class CarSpawn : MonoBehaviour
     public GameObject car3;
     public GameObject car4;
     public Transform transform;
+
+    private GameObject countDown;
+    public AudioSource threeSound;
+    public AudioSource twoSound;
+    public AudioSource oneSound;
+    public AudioSource goSound;
 
     // Start is called before the first frame update
     void Start()
@@ -37,5 +44,39 @@ public class CarSpawn : MonoBehaviour
             car.transform.position = transform.position;
             car.transform.rotation = transform.rotation;
         }
+
+        StartCoroutine(CountDownRoutine(car));
+    }
+
+    IEnumerator CountDownRoutine(GameObject car)
+    {
+        car.GetComponent<RCC_CarControllerV3>().enabled = false;
+        
+        countDown = GameObject.FindGameObjectWithTag("CntDown");
+        countDown.GetComponent<Text>().text = "";
+
+        yield return new WaitForSeconds(0.5f);
+        countDown.GetComponent<Text>().text = "3";
+        //threeSound.Play();
+        countDown.SetActive(true);
+
+        yield return new WaitForSeconds(1f);
+        countDown.SetActive(false);
+        countDown.GetComponent<Text>().text = "2";
+        //twoSound.Play();
+        countDown.SetActive(true);
+
+        yield return new WaitForSeconds(1f);
+        countDown.SetActive(false);
+        countDown.GetComponent<Text>().text = "1";
+        //oneSound.Play();
+        countDown.SetActive(true);
+
+        yield return new WaitForSeconds(1f);
+        countDown.SetActive(false);
+        car.GetComponent<RCC_CarControllerV3>().enabled = true;
+        countDown.GetComponent<Text>().text = "GO!";
+        //goSound.Play();
+        countDown.SetActive(true);
     }
 }
