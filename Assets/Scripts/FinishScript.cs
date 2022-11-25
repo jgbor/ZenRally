e2f308d9071349b7fb28f8d10f05f61655726b9b
jgbor <100523032+jgbor@users.noreply.github.com>
2022-11-25 11:29:29 +0100

    gombok jók

--- Assets/Scripts/FinishScript.cs
+++ Assets/Scripts/FinishScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishScript : MonoBehaviour
 {
@@ -11,5 +12,19 @@ public class FinishScript : MonoBehaviour
         GameObject.Find("Timer").GetComponent<TimingScript>().countTime = false;
         FinishMenu.SetActive(true);
         GameObject.FindGameObjectWithTag("Player").GetComponentInParent<RCC_CarControllerV3>().canControl = false;
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1;
+        AudioListener.pause = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Menu()
+    {
+        Time.timeScale = 1;
+        AudioListener.pause = false;
+        SceneManager.LoadScene(0);
     }
 }
